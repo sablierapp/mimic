8f48e6ec2f1dd408cebabd678561bbda5b37485c
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2024-10-22 00:28:52 -0400

    fix: add semantic release

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "branches":  [
+    { "name": "main" },
+  ],
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ]
+}
